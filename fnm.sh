4795945a1c78109d89471ae125bf06bb1a6a70d9
kyon48 <dygks1733@pusan.ac.kr>
2023-05-10 18:56:55 +0900

    make fnm

--- fnm.sh
+++ fnm.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
 function network_start {
-    sudo bash ./blockchain/core.sh all
+    sudo cp -rf ./chaincode ./fabric-network/src/asset
+    sudo bash ./fabric-network/network.sh up createChannel -ca
+    sudo bash ./fabric-network/network.sh up createChannel -ca
+    sudo bash ./fabric-network/network.sh deployCC -ccn basic -ccp src/asset/chaincode -ccl go
+}
+
+function network_clear {
+    sudo bash ./fabric-network/network.sh down
+    sudo rm -rf ./fabric-network/src/asset/chaincode
+    sudo rm -rf ./fabric-network/src/asset/channel-artifacts
 }
 
 # function network_invoke {
@@ -32,12 +43,12 @@ function network_start {
 # }
 
 function network_usage {
-    cecho "RED" "you should use start | clear | invoke | query | restart"
+    echo "RED" "you should use start | clear | invoke | query | restart"
 }
 
 function main {
     case $1 in
-        start )
+        start | clear)
             cmd=network_$1
             shift
             $cmd $@
